e156ec5c83b33e5a2f545cab09a712981790db81
Zachary Flower <zach@flower.codes>
2024-09-29 14:55:06 -0600

    add script

---
+++ ip.py
@@ -0,0 +1,8 @@
+import requests
+
+response = requests.get('https://canhazip.com')
+print("     |\__/,|   (`\\ ")
+print("   _.|o o  |_   ) ) ")
+print("+-(((---(((--------+")
+print(f"|{response.text.strip().center(18)}|")
+print("+------------------+")
